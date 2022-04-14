8118fb898e0ec23ff41d911ab440f5076dc4ab0c
PercTruman <drshawnhart@gmail.com>
2022-04-14 16:11:52 -0500

    all challenges complete, had to use mini functions

--- src/index.js
+++ src/index.js
@@ -25,30 +25,55 @@ function printBreedList(){
     fetch('https://dog.ceo/api/breeds/list/all')
     .then(res=>res.json())
     .then(dogList => {
-        breedArray = Object.keys(dogList.message)
-        breedArray.forEach(breed =>renderBreed(breed))
-                })
+        breeds = Object.keys(dogList.message)
+        updateBreed(breeds)
+        filterByFirstLetter()
+            })
 
-        function renderBreed(breed){
-        let imgElement = document.createElement('li')
-        imgElement.innerHTML = 
-        `${breed}`
-        document.getElementById('dog-breeds').appendChild(imgElement)
-        imgElement.addEventListener('click',()=>imgElement.style.color = 'aquamarine')
+
+        }
+
+function updateBreed(breeds){
+    let ul = document.querySelector('#dog-breeds')
+    removeChildren(ul)
+    breeds.forEach(breed =>renderBreed(breed))
+}
+
+function removeChildren(element){
+    let child = element.lastElementChild
+    while (child){
+        element.removeChild(child)
+        child = element.lastElementChild
     }
+
+}
+
+function filter(letter){
+    updateBreed(breeds.filter(breed=>breed.startsWith(letter)))
+
+}
+
+
+function renderBreed(breed){
+    let imgElement = document.createElement('li')
+    imgElement.innerHTML = 
+    `${breed}`
+    document.getElementById('dog-breeds').appendChild(imgElement)
+    imgElement.addEventListener('click',()=>imgElement.style.color = 'aquamarine')
 }
 
 
 
-// filterByFirstLetter()
 
-// function filterByFirstLetter(){
-//     // let letterSelector = document.querySelector('#breed-dropdown').addEventListener('change', (e)=>{
-//         if (e.target.value !== (breed[0])){
-//            document.querySelector('#dog-breeds').removeChild(imgElement)
-//            }})}
+function filterByFirstLetter(){
+    // const letter = e.target.value
+   document.querySelector('#breed-dropdown').addEventListener('change', (e)=>{
+       filter(e.target.value)
+        // if (e.target.value !== (breed[0])){
+        //    document.querySelector('#dog-breeds').removeChild(imgElement)
+        //    }})}
     // let chosenLetter = letterSelector.target.value
-    // console.log(letterSelector)
-    // letterSelector
+   })
+}
 
    
